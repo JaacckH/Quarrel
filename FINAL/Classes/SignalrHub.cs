06172bd345f6ef45790aa6e5e14aa9f778c7aa7b
JaacckH <75697218+JaacckH@users.noreply.github.com>
2021-04-24 22:54:17 +0100

    plan and design completed for index

---
+++ FINAL/Classes/SignalrHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FINAL.Classes
+{
+    public class SignalrHub : Hub
+    {
+    }
+}
--- FINAL/Pages/Index.cshtml.cs
+++ FINAL/Pages/Index.cshtml.cs
@@ -12,23 +12,6 @@ namespace Group_Project.Models
 
     public class IndexModel : PageModel
     {
-        public String getProductShowcase()
-        {
-            SqlConnection conn = new SqlConnection();
-            conn.ConnectionString = DBFunctions.connectionString;
-            conn.Open();
-            SqlCommand query = conn.CreateCommand();
-            query.CommandText = "SELECT TOP 20 * FROM Products";
-            SqlDataReader reader = query.ExecuteReader();
-
-            String html = "";
-            while (reader.Read())
-            {
-                html += ProductFunctions.getSubProductHtml(reader["ProductID"].ToString());
-            }
-
-            conn.Close();
-            return html;
-        }
+        
     }
 }
