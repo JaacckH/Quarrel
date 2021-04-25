96109b150966ffca9fba06b84cc8760a563888f7
JaacckH <75697218+JaacckH@users.noreply.github.com>
2021-04-25 12:11:21 +0100

    Message mockup done

---
+++ FINAL/Pages/Message.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FINAL.Classes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace Group_Project.Models
+{
+
+    public class MessageModel : PageModel
+    {
+        
+    }
+}
